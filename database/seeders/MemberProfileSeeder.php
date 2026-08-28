108ba8822035393013b5442ee3c65c0a4594ca7c
holmesshanea <holmesshanea@yahoo.com>
2026-08-27 20:42:57 -0400

    Seeder Modifications

--- database/seeders/MemberProfileSeeder.php
+++ database/seeders/MemberProfileSeeder.php
@@ -3,14 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\MemberProfile;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class MemberProfileSeeder extends Seeder
 {
     public function run(): void
     {
-        MemberProfile::factory()
-            ->count(75)
-            ->create();
+        $user = User::where('email', 'holmesshanea@yahoo.com')->firstOrFail();
+
+        MemberProfile::updateOrCreate(
+            ['user_id' => $user->id],
+            [
+                'username' => 'shaneholmes',
+                'profile_name' => 'Shane Holmes',
+                'city' => 'Saranac Lake',
+                'state_province' => 'New York',
+                'country' => 'United States',
+                'bio' => 'IRDI Member and responsible metal detectorist.',
+                'directory_visible' => true,
+            ]
+        );
     }
 }
