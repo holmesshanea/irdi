0979f1da8348d66c4f2322dcddd1aebfa4c39d60
holmesshanea <holmesshanea@yahoo.com>
2026-09-01 19:56:50 -0400

    SEO-03

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('irdi-logo.png') }}">
 
     {{-- SEO --}}
     <title>{{ $title ?? 'IRDI | International Responsible Detectorist Institute' }}</title>
